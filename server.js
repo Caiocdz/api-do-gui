ddc50588d981edade792f76317fde661737c087d
Caiocdz <caio.cdz.2jz@gmail.com>
2026-04-04 10:52:31 -0300

    informações no server.js

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`servidor rodando em http://localhost:${port}`);
 });
+
+//PORT=3000
+//DB_HOST=localhost
+//DB_USER=root
+//DB_PASSWORD=root
+//DB_NAME=corrida_db
+//DB_PORT=3306
