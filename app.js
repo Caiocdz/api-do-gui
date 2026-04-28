fff8916c1c3e131af9af875649dc15eca07965ab
Caiocdz <caio.cdz.2jz@gmail.com>
2026-04-28 07:46:52 -0300

    api feita

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
 
 app.use(express.json());
 
+// Configurar CORS
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+// Servir arquivos estáticos do frontend
+
+app.use(express.static(path.join(__dirname, '..', 'front-api')));
+
+// Rotas da API
 const usersRouter = require('./routes/users');
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 const corredoresRouter = require('./routes/corredores')
-app.use('/corredores',corredoresRouter)
+app.use('/api/corredores', corredoresRouter)
+
+// Rota raiz - servir index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'front-api', 'login.html'));
+});
 
 module.exports = app;
